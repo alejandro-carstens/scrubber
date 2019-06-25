7cdac0117c7ece093b56a723cf48f3de9c3c5c48
Alejandro Carstens Cattori <alejandro@stitchlabs.com>
2019-06-24 23:16:03 -0700

    adding open indices command.

---
+++ console/cmd/open_indices_cmd.go
@@ -0,0 +1,45 @@
+package cmd
+
+import (
+	"errors"
+	"scrubber/actions/contexts"
+	"scrubber/actions/options"
+	"scrubber/logging"
+
+	"github.com/spf13/cobra"
+)
+
+type openIndicesCmd struct {
+	baseActionCmd
+}
+
+func (oic *openIndicesCmd) new(logger *logging.SrvLogger) *cobra.Command {
+	command := &cobra.Command{
+		Use:   "open-indices",
+		Short: "open the specified list of indices",
+		Args:  oic.Validate,
+		Run:   oic.Handle,
+	}
+
+	command.Flags().StringSlice("indices", []string{}, "indices to be openned")
+
+	oic.logger = logger
+
+	return command
+}
+
+func (oic *openIndicesCmd) Validate(cmd *cobra.Command, args []string) error {
+	indices, _ := cmd.Flags().GetStringSlice("indices")
+
+	if len(indices) == 0 {
+		return errors.New("a list of indices is required to run this command")
+	}
+
+	oic.context = new(contexts.OpenIndicesContext)
+	oic.context.SetList(indices...)
+
+	options := &options.OpenIndicesOptions{}
+	options.BindFlags(cmd.Flags())
+
+	return oic.context.SetOptions(options)
+}
